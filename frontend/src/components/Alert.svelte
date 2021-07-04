9ae030130c03e28ba8af1b17add083f6f4fdeaaa
popunbom <28749104+popunbom@users.noreply.github.com>
2021-07-04 18:51:43 +0900

    Commonize style

--- frontend/src/components/Alert.svelte
+++ frontend/src/components/Alert.svelte
@@ -8,7 +8,10 @@
 </script>
 
 <div class="alert" transition:fly={{ y: -100 }}>
-  <div class="material-icons close" on:click={() => dispatcher("close")}>
+  <div
+    class="material-icons btn__icon_close"
+    on:click={() => dispatcher("close")}
+  >
     close
   </div>
   <div class="message">
@@ -34,16 +37,6 @@
     z-index: 100;
     box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.14),
       0 1px 18px 0 rgba(0, 0, 0, 0.12), 0 3px 5px -1px rgba(0, 0, 0, 0.2);
-  }
-  .close {
-    position: absolute;
-    right: 8px;
-    top: 8px;
-    color: rgba(0, 0, 0, 0.12);
-    cursor: pointer;
-    &:hover {
-      color: rgba(0, 0, 0, 0.48);
-    }
   }
   .message {
     margin-top: 24px;
