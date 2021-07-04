189ad6221e1faf17b08cfeec0377fa9b1385cddd
popunbom <28749104+popunbom@users.noreply.github.com>
2021-07-04 12:31:58 +0900

    add ResultCanvas

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,12 +1,35 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+
   import AppBar from "./components/AppBar.svelte";
   import PointsSelector from "./components/PointsSelector.svelte";
   import { basePointSelector, overPointSelector } from "./stores";
+  import ResultCanvas from "./components/ResultCanvas.svelte";
+  import { sendTransformRequest } from "./utils/api";
+
+  let resultCanvasDom: HTMLDivElement;
+  let resultCanvasWidth: number;
+  let resultCanvasHeight: number;
+  let transformedImage: HTMLImageElement;
+
+  const handleTransform = () => {
+    console.debug("handleTransform");
+    sendTransformRequest($basePointSelector, $overPointSelector).then(
+      (image) => {
+        transformedImage = image;
+        console.debug(transformedImage);
+      },
+    );
+  };
+  onMount(() => {
+    resultCanvasWidth = resultCanvasDom.clientWidth;
+    resultCanvasHeight = resultCanvasDom.clientHeight;
+  });
 </script>
 
 <div id="app">
   <header>
-    <AppBar />
+    <AppBar on:transform={() => handleTransform()} />
   </header>
   <main>
     <div class="point-selectors">
@@ -16,6 +39,14 @@
       <div>
         <PointsSelector name="変形画像" bind:data={$overPointSelector} />
       </div>
+    </div>
+    <div class="result-canvas" bind:this={resultCanvasDom}>
+      <ResultCanvas
+        width={resultCanvasWidth}
+        height={resultCanvasHeight}
+        baseImage={transformedImage}
+        overImage={$basePointSelector.image}
+      />
     </div>
   </main>
 </div>
@@ -30,10 +61,15 @@
   main {
     height: 93%;
     position: relative;
+    display: flex;
+    justify-content: space-between;
+    padding: 16px;
+    // border: 2px solid cornflowerblue;
+    box-sizing: border-box;
   }
   .point-selectors {
+    width: 59%;
     height: 100%;
-    padding: 16px;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
@@ -41,5 +77,12 @@
     & > * {
       height: 49%;
     }
+  }
+  .result-canvas {
+    width: 40%;
+    height: 100%;
+    // padding: 16px;
+    // border: 2px solid gray;
+    box-sizing: border-box;
   }
 </style>
---
+++ frontend/src/components/ResultCanvas.svelte
@@ -0,0 +1,108 @@
+<script lang="ts">
+  import { tick } from "svelte";
+  import type Konva from "konva";
+
+  import { calcStageConfig } from "../utils/configs";
+  import { newImage } from "../entities/Image";
+  import type { ScalableStageConfig } from "../utils/configs";
+  import type { LayerChildType } from "./canvas/ScalableStage.svelte";
+
+  import ScalableStage from "./canvas/ScalableStage.svelte";
+
+  // Props
+  export let width: number;
+  export let height: number;
+  export let baseImage: HTMLImageElement;
+  export let overImage: HTMLImageElement;
+
+  // Fixed data
+  // let scalableStageConfig = calcStageConfig(width, height, baseImage);
+
+  // Reactive data
+  let stage: Konva.Stage;
+  let layer: Konva.Layer;
+  let childs: LayerChildType[] = [];
+  let opacity: number = 0.5;
+  let visibleOverImage: boolean = true;
+
+  $: kBaseImage = newImage({
+    image: baseImage,
+    opacity: 1,
+  });
+
+  $: kOverImage = newImage({
+    image: overImage,
+    opacity: opacity,
+    visible: visibleOverImage,
+  });
+
+  // Computed values
+  $: childs = (() => {
+    console.log("kBaseImage, kOverImage");
+    console.debug(kBaseImage);
+    console.debug(kOverImage);
+    return [kBaseImage, kOverImage];
+  })();
+  $: disabled = baseImage === undefined || overImage === undefined;
+  $: scalableStageConfig = (() => {
+    console.log("re-calc stage config");
+    return calcStageConfig(width, height, baseImage);
+  })();
+
+  const handleOpacityChange = (newValue: number) => {
+    kOverImage.opacity(newValue);
+  };
+  const handleToggleVisible = (newVisibleOverImage: boolean) => {
+    kOverImage.visible(newVisibleOverImage);
+    visibleOverImage = newVisibleOverImage;
+  };
+</script>
+
+<div class="result-canvas">
+  <div class="toolbar">
+    <label for="opacity">Opacity</label>
+    <input
+      type="range"
+      name="opacity"
+      id="opacity"
+      min="0"
+      max="1"
+      step="0.01"
+      {disabled}
+      bind:value={opacity}
+      on:change={({ currentTarget }) =>
+        handleOpacityChange(currentTarget.valueAsNumber)}
+    />
+    <button {disabled} on:click={() => handleToggleVisible(!visibleOverImage)}
+      >{visibleOverImage ? "hidden" : "visible"}</button
+    >
+  </div>
+  <div class="scalable-stage">
+    <ScalableStage
+      bind:stage
+      bind:layer
+      {childs}
+      config={scalableStageConfig}
+    />
+  </div>
+</div>
+
+<style lang="scss">
+  @import "../stylesheets/global";
+
+  .result-canvas {
+    height: 100%;
+  }
+
+  .toolbar {
+    @extend .flex__center_vertical;
+    height: 5%;
+    & > * {
+      margin-right: 1em;
+    }
+  }
+  .scalable-stage {
+    border: 2px solid gray;
+    height: 95%;
+  }
+</style>
